a9f269d34a29c59501824b851adb59b9f01d97b0
Amzzz <86222462+iAmzzz278@users.noreply.github.com>
2025-06-17 05:53:14 -0600

    PLSSSS

--- pageScript/script.js
+++ pageScript/script.js
@@ -1,3 +1,14 @@
 function resetearTextarea() {
     document.getElementById("textarea").value = "";
   }
+
+  document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_fptsx6q', 'template_7dvfbbs', this)
+    .then(function() {
+      alert('Correo enviado con éxito!');
+    }, function(error) {
+      alert('Error al enviar: ' + JSON.stringify(error));
+    });
+});
