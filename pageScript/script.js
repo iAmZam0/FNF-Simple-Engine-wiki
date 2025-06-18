fad09102b1749e96ed1fb67ea2ccfd3aad337371
Amzzz <86222462+iAmzzz278@users.noreply.github.com>
2025-06-17 20:45:47 -0600

    FOOTER FCKER

--- pageScript/script.js
+++ pageScript/script.js
@@ -2,12 +2,3 @@ function resetearTextarea() {
     document.getElementById("textarea").value = "";
   }
 
-emailjs.init("Yp9B5u-2aHaXMIKli");
-
-  document.getElementById("my-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    emailjs.sendForm("service_fptsx6q", "template_7dvfbbs", this)
-      .then(() => alert("Correo enviado correctamente"), 
-            (err) => alert("Error: " + JSON.stringify(err)));
-  });
